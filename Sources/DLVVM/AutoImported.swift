f5d6c0aa2a94eda3403e4e6c1911adcf6fe50cb6
Devin Lai <devin.lai@icardinalblue.com>
2024-07-18 00:06:14 +0800

    feat: Add DLVVM

---
+++ Sources/DLVVM/AutoImported.swift
@@ -0,0 +1,9 @@
+//
+//  AutoImported.swift
+//
+//
+//  Created by 賴柏宏 on 2024/7/17.
+//
+
+@_exported import SwiftUI
+@_exported import Combine
---
+++ Sources/DLVVM/DLView.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public typealias DLView = DevinLaiView
+
+public protocol DevinLaiView: View {
+  associatedtype ViewModel: DevinLaiViewModel
+
+  var viewModel: ViewModel { get }
+
+  var observation: ViewModel.ViewObservation { get }
+
+  init(viewModel: ViewModel)
+}
